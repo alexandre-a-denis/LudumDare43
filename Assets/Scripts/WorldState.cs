aee35eb2bbe2d03a8c54cb6ae706516e143b7348
Raphaël DOUYERE <rdouyere@users.noreply.github.com>
2018-12-01 14:25:04 +0100

    Agian

--- Assets/Scripts/WorldState.cs
+++ Assets/Scripts/WorldState.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Not linked to Unity stuff (at least for now)
-// Contains all global states as well as uptdates on them.
+// Container for "global" states
 public class WorldState  {
     
-
 }
