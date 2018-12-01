e0a4883845e43b4ed875c7b4cda6a8a1ca4bbfbb
Raphaël DOUYERE <rdouyere@users.noreply.github.com>
2018-12-01 16:02:44 +0100

    More on WorldState

--- Assets/Scripts/WorldState.cs
+++ Assets/Scripts/WorldState.cs
@@ -7,7 +7,7 @@ public class WorldState : MonoBehaviour
 {
 
     // Number of turns before game ends.
-    private int turnCountLimit = 20;
+    public int turnCountLimit = 20;
 
     // Current turn count.
     public int turnCount = 1;
@@ -16,7 +16,10 @@ public class WorldState : MonoBehaviour
     public bool end = false;
 
     // Defines the number of human in the ship.
-    int crewSize;
+    public int totalCrew;
+
+    // Crew that is not assigned to any room
+    public int unassignedCrew;
 
     // Number of food units (each crew member uses 1 food per turn). If no food is left, crew will starve/die.
     int foodAmount;
@@ -24,15 +27,27 @@ public class WorldState : MonoBehaviour
     // Level of hope. Hope is not "consumed" like food is. It will affect capacity of crew during events.
     int hopeLevel;
 
+
+    // Rooms & crew in them
+
+
+
     // Increase turn count. Perform "end of turn"
     public void NextTurn()
     {
         // Consume food (1 unit per crew)
-        foodAmount -= crewSize;
+        foodAmount -= totalCrew;
         if (foodAmount < 0)
         {
-            // No more food, you loose
             end = true;
+            Debug.Log("Negative food, you loose");
+        }
+
+        // Check if some crew remains
+        if (totalCrew <= 0)
+        {
+            end = true;
+            Debug.Log("Negative crew (all people died), you loose");
         }
 
         if (!end)
@@ -44,11 +59,52 @@ public class WorldState : MonoBehaviour
             else
             {
                 end = true;
-                Debug.Log("Reached max turn");
+                Debug.Log("Reached max turn, you win");
             }
         }
     }
 
+    // =============== Room management =====================
 
+    private Dictionary<int, Room> rooms = new Dictionary<int, Room>();
+
+    private void InitRooms()
+    {
+        int roomId = 0;
+        int nbFoodRooms = 3;
+        int nbHopeRooms = 3;
+
+        for (int i = 0; i < nbFoodRooms; i++)
+        {
+            rooms.Add(roomId++, new Room("food"));
+        }
+    }
+
+
+    public void AddCrewToRoom(string roomId, int nbCrewToAssign)
+    {
+        if (nbCrewToAssign > unassignedCrew)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public struct Room
+    {
+        public int crew;
+        public string type; // TODO have an enum?
+
+        public Room(string t)
+        {
+            crew = 0;
+            type = t;
+        }
+
+
+    }
 
 }
