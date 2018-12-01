b76f4f0db15ba86e95e4daed21986180e32bf880
Raphaël DOUYERE <rdouyere@users.noreply.github.com>
2018-12-01 14:17:20 +0100

    Add new script file

---
+++ Assets/Scripts/WorldState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Not linked to Unity stuff (at least for now)
+// Contains all global states as well as uptdates on them.
+public class WorldState  {
+    
+
+}
