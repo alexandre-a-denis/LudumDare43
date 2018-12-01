ee1d30dda5717af83e1bb7dfe93ed63db5df89ae
Alexandre Denis <alexandre.a.denis@gmail.com>
2018-12-01 16:35:49 +0100

    Added prefabs

---
+++ Assets/Scripts/Tests/CreateRoomTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRoomTest : MonoBehaviour
+{
+    public Room roomPrefab;
+    public GameObject canvas;
+
+	void Start () {
+        Room wreckClone = (Room) Instantiate(roomPrefab, Vector3.zero, Quaternion.identity) as Room;
+        wreckClone.transform.SetParent(canvas.transform);
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
