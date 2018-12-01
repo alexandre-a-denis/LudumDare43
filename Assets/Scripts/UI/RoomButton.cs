e691ec5de3a6a8673e9d880d8f901255df1adc9e
Alexandre Denis <alexandre.a.denis@gmail.com>
2018-12-01 17:27:46 +0100

    Added all button behaviour

---
+++ Assets/Scripts/UI/RoomButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomButton : RoomUIScript
+{
+    public int increment = 1;
+    Button button;
+
+    // Use this for initialization
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        room.AddCrew(increment);
+        Debug.Log("Added " + increment + " to " + room.ToString());
+    }
+}
--- Assets/Scripts/UI/RoomCrewValue.cs
+++ Assets/Scripts/UI/RoomCrewValue.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RoomCrewValue : MonoBehaviour
+public class RoomCrewValue : RoomUIScript
 {
-    Room parentRoom;
     Text text;
 
     // Use this for initialization
     void Start()
     {
         text = GetComponent<Text>();
-        parentRoom = GetComponentInParent<Room>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        text.text = parentRoom.numberOfCrew.ToString();
+    { 
+        text.text = room.numberOfCrew.ToString();
     }
 }
