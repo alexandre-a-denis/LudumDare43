f02717c4a2d0539b99bbecc00d996d1cc940b608
Alexandre Denis <alexandre.a.denis@gmail.com>
2018-12-01 16:19:35 +0100

    Added Room

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoomType { FOOD, RELICS, REST }
+
+public enum RoomStatus { OPERATIONAL, DESTROYED }
+
+// A Room
+public class Room : MonoBehaviour
+{
+
+    public int numberOfCrew;
+    public int resourcesNb;
+    public RoomType roomType;
+    public RoomStatus roomStatus;
+
+    
+
+    public void AddCrew(int number)
+    {
+        numberOfCrew += number;
+    }
+
+
+
+}
