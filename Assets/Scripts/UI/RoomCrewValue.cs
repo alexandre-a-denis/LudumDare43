683d42bffcd8deb95d92396c58e6038caf2a0e5a
Alexandre Denis <alexandre.a.denis@gmail.com>
2018-12-01 17:03:16 +0100

    Added RoomCrewValue

---
+++ Assets/Scripts/UI/RoomCrewValue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomCrewValue : MonoBehaviour
+{
+    Room parentRoom;
+    Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        parentRoom = GetComponentInParent<Room>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = parentRoom.numberOfCrew.ToString();
+    }
+}
