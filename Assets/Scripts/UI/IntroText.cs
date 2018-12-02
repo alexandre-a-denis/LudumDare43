d8dd871d7b12b233736e65e7a1f95c599aea9977
Alexandre Denis <alexandre.a.denis@gmail.com>
2018-12-02 16:40:42 +0100

    Removed hope

---
+++ Assets/Scripts/UI/IntroText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndText : MonoBehaviour
+{
+    Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Text>().text = IAText.Intro();
+    }
+}
