9da33d1d7350003fc0153a3b472c9e8d1eff0b27
ByGGy <lebyggy@gmail.com>
2018-12-02 17:30:17 +0100

    Refactoring Prediction and Solver for new sacrifice

--- Assets/Scripts/DramaOutcomePrediction.cs
+++ Assets/Scripts/DramaOutcomePrediction.cs
@@ -4,82 +4,42 @@ using UnityEngine;
 using System.Linq;
 using System.Text;
 
-public class DramaOutcomeSample
-{
-	private readonly int resourceQty;
-	private readonly int crewQty;
-
-	private readonly bool willRoomBeDestroyed;
-	private readonly int resourceQtyLoss;
-	private readonly int crewQtyLoss;
-
-	public DramaOutcomeSample(int resourceQty, int crewQty, bool willRoomBeDestroyed, int resourceQtyLoss, int crewQtyLoss)
-	{
-		this.resourceQty = resourceQty;
-		this.crewQty = crewQty;
-
-		this.willRoomBeDestroyed = willRoomBeDestroyed;
-		this.resourceQtyLoss = resourceQtyLoss;
-		this.crewQtyLoss = crewQtyLoss;
-	}
-
-	public int ResourceQty { get {return this.resourceQty;} }
-	public int CrewQty { get {return this.crewQty;} }
-
-	public bool WillRoomBeDestroyed { get {return this.willRoomBeDestroyed;} }
-	public int ResourceQtyLoss { get {return this.resourceQtyLoss;} }
-	public int CrewQtyLoss { get {return this.crewQtyLoss;} }	
-}
-
 public class DramaOutcomePrediction
-{
-	private const int MAX_SAMPLES = 100;
+{	
+	private const int MAX_INITIAL_CREW = 50;
 
 	private readonly Drama drama;
-	private readonly List<DramaOutcomeSample> samples;
+	private readonly int crewQtyToSacrifice;
 
-	public DramaOutcomePrediction(Drama drama, int escapeCrewQty, float currentHope)
+	public DramaOutcomePrediction(Drama drama, int crewQtyToSacrifice, float currentHope)
 	{
+		// Fuck Hope, we do not need THAT !
 		this.drama = drama;
-		this.samples = Enumerable.Range(0, MAX_SAMPLES).Select(sampleIndex => GenerateSample(escapeCrewQty, currentHope)).ToList();
-	}
-
-	private DramaOutcomeSample GenerateSample(int escapeCrewQty, float currentHope)
-	{
-		int resourceQty = this.drama.Room.resourcesNb;
-		int crewQty = this.drama.Room.numberOfCrew;
-
-		int sacrificingCrewQty = crewQty - escapeCrewQty;
-		float sacrificingCrewFactor = (float)System.Math.Sqrt(sacrificingCrewQty);
-		bool willRoomBeDestroyed = (Random.Range(0.0f, 1.0f) * sacrificingCrewFactor * currentHope) < 1.0f;
-
-		return new DramaOutcomeSample(resourceQty, crewQty, willRoomBeDestroyed, willRoomBeDestroyed ? resourceQty : 0, sacrificingCrewQty);
+		this.crewQtyToSacrifice = crewQtyToSacrifice;
 	}
 
 	public Drama Drama { get {return this.drama;} }
+	public int CrewQtyToSacrifice { get {return this.crewQtyToSacrifice;} }
 
-	public DramaOutcomeSample PickOneSample()
+	// Normalized between [0.0f, 1.0f]
+	public float EvaluateBaseProbabilityToSaveRoom()
 	{
-		return this.samples[Random.Range(0, this.samples.Count)];
+		//sqrt(10 000) = 100
+		int crewQty = this.drama.Room.numberOfCrew;
+		float crewUnitFactor = 10000 / MAX_INITIAL_CREW;
+		return (float)System.Math.Sqrt(crewQty * crewUnitFactor) / 100.0f;
 	}
 
-	public float EvaluateProbabilityToLooseRoom()
+	// Normalized between [0.0f, 1.0f]
+	public float EvaluateTotalProbabilityToSaveRoom()
 	{
-		return this.samples.Count(sample => sample.WillRoomBeDestroyed) / (float)this.samples.Count;
-	}
-
-	public float EvaluateProbabilityToLooseResources()
-	{
-		return this.samples.Count(sample => sample.ResourceQtyLoss > 0) / (float)this.samples.Count;
-	}
-
-	public float EvaluateProbabilityToLooseCrew()
-	{
-		return this.samples.Count(sample => sample.CrewQtyLoss > 0) / (float)this.samples.Count;
+		int crewQty = this.drama.Room.numberOfCrew;
+		float sacrificeCrewBonus = System.Math.Min(crewQty, this.crewQtyToSacrifice) * 0.1f; 
+		return System.Math.Min(EvaluateBaseProbabilityToSaveRoom() + sacrificeCrewBonus, 1.0f);
 	}
 
 	public override string ToString()
 	{
-		return string.Format("{0}% to loose room | {1}% to loose resources | {2}% to loose crew", EvaluateProbabilityToLooseRoom() * 100, EvaluateProbabilityToLooseResources() * 100, EvaluateProbabilityToLooseCrew() * 100);
+		return string.Format("Chance to save Room = {0}% + {1} crew members sacrified = {2}%)", EvaluateBaseProbabilityToSaveRoom() * 100, this.crewQtyToSacrifice, EvaluateTotalProbabilityToSaveRoom() * 100);
 	}
 }
--- Assets/Scripts/DramaReport.cs
+++ Assets/Scripts/DramaReport.cs
@@ -8,25 +8,28 @@ public class DramaReport
 	private readonly bool hasRoomBeenDestroyed;
 	private readonly int resourceQtyLoss;
 	private readonly int crewQtyLoss;
+	private readonly int crewQtySacrified;
 
-	public DramaReport(Room room, int resourceQtyLoss, int crewQtyLoss)
+	public DramaReport(Room room, int resourceQtyLoss, int crewQtyLoss, int crewQtySacrified)
 	{
 		this.roomType = room.roomType;
 		this.hasRoomBeenDestroyed = room.roomStatus == RoomStatus.DESTROYED;
 		this.resourceQtyLoss = resourceQtyLoss;
 		this.crewQtyLoss = crewQtyLoss;
+		this.crewQtySacrified = crewQtySacrified;
 	}
 
 	public RoomType RoomType { get {return this.roomType;} }
 	public bool HasRoomBeenDestroyed { get {return this.hasRoomBeenDestroyed;} }
 	public int ResourceQtyLoss { get {return this.resourceQtyLoss;} }
 	public int CrewQtyLoss { get {return this.crewQtyLoss;} }	
+	public int CrewQtySacrified { get {return this.crewQtySacrified;} }
 
 	public override string ToString()
 	{
 		if (this.hasRoomBeenDestroyed)
-			return string.Format("{0} of {1} lost | {2} crew members lost", this.resourceQtyLoss, this.roomType, this.crewQtyLoss);
+			return string.Format("{0} of {1} lost | {2} crew members lost ({3} of em has been sacrified)", this.resourceQtyLoss, this.roomType, this.crewQtyLoss, this.crewQtySacrified);
 
-		return string.Format("{0} crew members lost", this.crewQtyLoss);
+		return string.Format("Room has been saved; but {1} has been sacrified for it", this.crewQtySacrified);
 	}
 }
