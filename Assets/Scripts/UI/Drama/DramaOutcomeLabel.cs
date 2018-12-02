98f9f8acb63b2ff513cf8ca0d9da88a898f15884
Alexandre Denis <alexandre.a.denis@gmail.com>
2018-12-02 12:17:40 +0100

    Added outcome

---
+++ Assets/Scripts/UI/Drama/DramaOutcomeLabel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DramaOutcomeLabel : MonoBehaviour
+{
+    Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+   
+    void Update()
+    {
+        if (GameManager.manager.CurrentPhase == TurnPhases.MOVE)
+        {
+            if (GameManager.manager.CurrentDramaReport != null)
+                text.text = GameManager.manager.CurrentDramaReport.ToString();
+        }
+
+       // Debug.Log(GameManager.manager.CurrentPhase + " " + GameManager.manager.CurrentDrama);
+    }
+}
---
+++ Assets/Scripts/UI/Drama/DramaPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// activates one or the other of subpanels
+public class DramaPanel : MonoBehaviour
+{
+    public GameObject dramaChoicePanel;
+    public GameObject dramaOutcomePanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        dramaChoicePanel.SetActive(GameManager.manager.CurrentPhase == TurnPhases.DRAMA);
+        dramaOutcomePanel.SetActive(GameManager.manager.CurrentPhase == TurnPhases.MOVE);
+    }
+}
