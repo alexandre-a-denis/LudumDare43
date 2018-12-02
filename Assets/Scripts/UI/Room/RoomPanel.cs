4d0459a62fbf4ded41e7f43575b84277a7759a55
Alexandre Denis <alexandre.a.denis@gmail.com>
2018-12-02 08:43:27 +0100

    Added MainSceneUITests and changed Room

---
+++ Assets/Scripts/UI/Room/RoomPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// overall RoomPanel, disable room if not operational
+public class RoomPanel : RoomUIScript
+{
+    bool deactivated;
+    Image roomBackground;
+
+    void Start()
+    {
+        roomBackground = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (room.roomStatus == RoomStatus.DESTROYED)
+        {
+            if (!deactivated)
+            {
+                deactivated = true;
+                roomBackground.color = Color.red;
+         
+                foreach (Transform child in transform)
+                    child.gameObject.SetActive(false);
+            }
+        }
+    }
+}
